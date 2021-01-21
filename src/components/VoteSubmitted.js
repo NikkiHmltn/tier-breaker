4830690d0835a8511a926047930e8feea28cf6b7
Ruben Cedeno <rubencedeno@Rubens-MBP.verizon.net>
2021-01-21 11:54:21 -0500

    [Vote Submitted] Add route for submitted vote.

---
+++ src/components/VoteSubmitted.js
@@ -0,0 +1,9 @@
+function VoteSubmitted() {
+    return (
+      <div className="VoteSubmitted">
+        <h1>Thank you for submitting your votes!</h1>
+      </div>
+    );
+  }
+  
+  export default VoteSubmitted;
