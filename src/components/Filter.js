70090dd38252a0d4efe66f02adae991b22f4318c
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-22 16:30:36 -0500

    add month filter

---
+++ src/components/Filter.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function Filter(props) {
+    const [hidden, setHidden] = useState('hidden');
+
+    const handleClick = () => {
+        if (hidden === 'hidden') {
+            setHidden('shown');
+        } else {
+            setHidden('hidden');
+        }
+    };
+
+    const months = [
+        'Show All',
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    const monthDisp = months.map((month) => {
+        return (
+            <p key={month} onClick={() => props.onClick(month)}>
+                {month}
+            </p>
+        );
+    });
+
+    return (
+        <>
+            <div className={`${hidden} Filter-popup`}>{monthDisp}</div>
+
+            <div className="Filter-bubble" onClick={() => handleClick()}>
+                <i class="material-icons">filter_list</i>
+            </div>
+        </>
+    );
+}
+
+export default Filter;
