358ca76a830a693ed597984d6952adaef671d2a0
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-22 15:10:03 -0500

    fix multiform;

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from 'react-router-dom';
-import '../components/Navbar.css';
+import './css/Navbar.css';
 
 function Navbar() {
     return (
--- src/components/Public.js
+++ src/components/Public.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import './css/Public.css';
 
 class Public extends Component {
     constructor(props) {
@@ -19,14 +20,20 @@ class Public extends Component {
     render() {
         const brackets = this.state.publicBrackets.map((bracket) => {
             return (
-                <div>
+                <Link key={bracket.key} className="bracket-card" to={`/bracket/${bracket.key}`}>
+                    <p>{new Date(bracket.created_at.$date).toString().split(' ').slice(1, 4).join(' ')}</p>
                     <h3>{bracket.title}</h3>
                     <Link to={`/bracket/${bracket.key}`}>Vote!</Link>
-                </div>
+                </Link>
             );
         });
 
-        return <div>{brackets}</div>;
+        return (
+            <>
+                <h1>Public Tournaments</h1>
+                <div className="public-brackets">{brackets}</div>
+            </>
+        );
     }
 }
 
