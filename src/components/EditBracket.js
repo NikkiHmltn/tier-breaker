83352afbe37eb251fe83922b20037e2ccc1162b4
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-01-23 13:17:31 -0800

    finished delete and put route for edit page. tested. works

--- src/components/EditBracket.js
+++ src/components/EditBracket.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom'
 import axios from 'axios'
 import M from "materialize-css"
 
@@ -10,58 +11,90 @@ export default class EditBracket extends Component {
       private: "",
       time_duration: "", 
       title: "",
+      rounds: ''
     }
   }
 
   componentDidMount() {
     M.AutoInit(); 
-    //get axios the bracket manually
-    //get it to display and fill in input values autom
-    axios.get(`${process.env.REACT_APP_SERVER_URL}/bracket/e71013dc`)
+   
+    this.setState({loading: true})
+    // DONT FORGET TO CHANGE THE BRACKET ID TO WHATEVER RUBEN HAS IT SET TO 
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/bracket/d10365ec`)
+    //SERIOUSLY DONT FORGET 
     .then((bracket) => {
       console.log(bracket)
       let startBracket = bracket.data
       this.setState({
         key: startBracket.key, 
         private: startBracket.private,
-        time_duration: startBracket.time_duration,
         title: startBracket.title,
+        redirect: false, 
+        loading: false
       })
     })
   }
 
-  //create a function to handle the PUT route and send that package back to the behind
-
-
   handleChange = (event) => {
     const {name, value} = event.target
-    console.log(name, value)
-    if (name === 'private' && value === "on") {
-      console.log('in here')
-      this.setState({private: true})
-    } else if (name=== 'public' && value === 'on'){
-      console.log('in here')
-      this.setState({private: false})
-    } else if (name=== 'public' && value === 'no') {
-      this.setState({private: true})
-    }
-    
+
     this.setState({
       [name]: value
     })   
   }
 
-  render(){
+  handlePrivate = (event) => {
+    const {name, value} = event.target
+    if (name === 'private' && value === "on") {
+      this.setState({private: true})
+    } else if (name=== 'public' && value === 'on'){
+      this.setState({private: false})
+    } 
+  }
 
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({loading: true})
+      const newBracket = {
+        title: this.state.title,
+        private: this.state.private,
+      }
+      axios.put(`${process.env.REACT_APP_SERVER_URL}/bracket/${this.state.key}/edit`, newBracket)
+      .then((newBracket) => {
+        this.setState({redirect: true, loading: false})
+      })
+  }
+
+  handleDelete = (event) => {
+    axios.delete(`${process.env.REACT_APP_SERVER_URL}/bracket/${this.state.key}/delete`)
+    .then((res)=> {
+      if (typeof res.data.msg === 'string' && res.data.msg.includes('deleted')) {
+        this.setState({ error: false, loading: false, redirect: true});
+      } else {
+        this.setState({ loading: false, error: true });
+      }
+    })
+  }
+
+  render(){
+    if (this.state.redirect) {
+      return <Redirect to='/' />
+    }
+    if (this.state.loading) {
+      <div>
+        LOADING....
+      </div>
+    }
     return (
       <div className="EditBracket">
         <h2>{this.state.key}</h2>
-        {this.state.private == true ? (
+        <form onSubmit={this.handleSubmit}>
+          {this.state.private == true ? (
           <div>
           <h4>Currently: Private</h4>
           <p>
             <label htmlFor="public">
-              <input type="checkbox" name="public" onChange={this.handleChange} />
+              <input type="checkbox" name="public" onChange={this.handlePrivate} />
               <span>Make Public</span>
             </label>
           </p>
@@ -71,7 +104,7 @@ export default class EditBracket extends Component {
           <h4>Currently: Public</h4>
           <p>
           <label>
-            <input htmlFor="private" type="checkbox" name="private" onChange={this.handleChange} />
+            <input htmlFor="private" type="checkbox" name="private" onChange={this.handlePrivate} />
             <span>Make Private</span>
           </label>
         </p>
@@ -86,20 +119,12 @@ export default class EditBracket extends Component {
             value={this.state.title}
             onChange={this.handleChange} 
           />
-        <h4>Current Time: {this.state.time_duration} </h4>  
-        <div className="input-field">
-          <select name="duration" id="duration"onChange={this.handleChange}>
-            <option>Select One</option>
-            <option value="1">1 day</option>
-            <option value="2">2 days</option>
-            <option value="3">3 days</option>
-            <option value="4">4 days</option>
-            <option value="5">5 days</option>
-            <option value="6">6 days</option>
-            <option value="7">7 days</option>
-          </select>
-          <label htmlFor="duration">How long do you want your poll to stay open for?</label>
-        </div>
+          <a class="waves-effect waves-light btn pink" onClick={this.handleSubmit}>Submit</a>
+          <a class="waves-effect waves-light btn pink" onClick={this.handleDelete}>DELETE</a>
+        </form>
+        
+
+
          
       </div>
     );
