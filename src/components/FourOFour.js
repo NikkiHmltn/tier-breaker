039dbd010867c5754f2243e64c2c4a5c7766a9be
Ruben Cedeno <rubencedeno@me.com>
2021-01-25 04:32:32 -1000

    [404 Page] Fix aligning of image.

--- src/components/FourOFour.js
+++ src/components/FourOFour.js
@@ -6,9 +6,11 @@ function FourOFour() {
       <div className="FourOFour">
         <h1>Oops! Looks like something went wrong.</h1>
         <h3 className="message">Our Devs are looking into this error. In the meantime, please go back to our homepage.</h3>
+        <div>
         <Link to="/">
         <img alt="" src="https://i.imgur.com/V89AznE.png"/>
         </Link>
+        </div>
       </div>
     );
   }
