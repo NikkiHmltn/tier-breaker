a7f43902b6cb20cceeed046ddb85f917475359af
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-25 20:09:43 -0500

    add end display for completed

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -6,17 +6,18 @@ import VoteSubmitted from './VoteSubmitted';
 import './css/Bracket.css';
 
 export default class Bracket extends Component {
-
-     constructor(props) {
+    constructor(props) {
         super(props);
         this.state = {
             socket: null,
             key: this.props.history.location.state.key || this.props.location.state,
             voting: [{}],
+            options: [],
             loading: true,
             title: '',
             error: false,
             redirect: false,
+            end_display: [],
             voted: ''
         };
     }
@@ -49,22 +50,35 @@ export default class Bracket extends Component {
                                 console.log(response);
                                 this.setState({
                                     key: response.data.bracket.key,
-                                    voting:
-                                        response.data.bracket.voting_options.votes[
-                                            response.data.bracket.voting_options.votes.length - 1
-                                        ],
+                                    voting: response.data.bracket.voting_options.votes,
+                                    options: response.data.bracket.voting_options.round_options,
                                     loading: false,
                                     title: response.data.bracket.title
                                 });
+                                if (response.data.bracket.end_display)
+                                    this.setState({
+                                        end_display:
+                                            response.data.bracket.end_display.winner ||
+                                            response.data.bracket.end_display.full_bracket ||
+                                            response.data.bracket.end_display.top_three
+                                    });
                             })
                             .catch((err) => this.setState({ redirect: true, loading: false }));
                     } else {
                         this.setState({
                             key: res.data.key,
-                            voting: res.data.voting_options.votes[res.data.voting_options.votes.length - 1],
+                            voting: res.data.voting_options.votes,
+                            options: res.data.voting_options.round_options,
                             loading: false,
                             title: res.data.title
                         });
+                        if (res.data.end_display)
+                            this.setState({
+                                end_display:
+                                    res.data.end_display.full_bracket ||
+                                    res.data.end_display.top_three ||
+                                    res.data.end_display.winner
+                            });
                     }
                 }
             })
@@ -72,12 +86,12 @@ export default class Bracket extends Component {
                 this.setState({ redirect: true, loading: false });
             });
 
-      socket.on('vote_cast', (data) => {
-          if (data.key === this.state.key) {
-              axios.get(`${process.env.REACT_APP_SERVER_URL}/bracket/${this.state.key}`).then((res) => {
-                console.log(res);
-                this.setState({ voting: res.data.voting_options.votes[res.data.voting_options.votes.length - 1] });
-              });
+        socket.on('vote_cast', (data) => {
+            if (data.key === this.state.key) {
+                axios.get(`${process.env.REACT_APP_SERVER_URL}/bracket/${this.state.key}`).then((res) => {
+                    console.log(res);
+                    this.setState({ voting: res.data.voting_options.votes });
+                });
             }
         });
     }
@@ -115,23 +129,76 @@ export default class Bracket extends Component {
             return <p>LOADING...</p>;
         }
         let key = [];
-        for (let k in this.state.voting) {
-            key.push(
-                <div className="voting" key={k}>
-                    <p>
-                        {k}: {this.state.voting[k]}
-                        <br />
-                        <button
-                            className={`waves-effect waves-light btn ${this.state.voted}`}
-                            onClick={(e) => {
-                                this.handleSubmit(e, k);
-                            }}>
-                            Vote
-                        </button>
-                    </p>
-                </div>
-            );
+        if (!this.state.end_display) {
+            for (let i = 0; i < this.state.voting.length; i = i + 2) {
+                key.push([]);
+                if (i === this.state.voting.length - 1) {
+                    for (let k of this.state.options) {
+                        key[i].push(
+                            <div className="voting" key={k}>
+                                <p>
+                                    {k}: {this.state.voting[i][k]}
+                                    <br />
+                                    <button
+                                        className={`waves-effect waves-light btn ${this.state.voted}`}
+                                        onClick={(e) => {
+                                            this.handleSubmit(e, k);
+                                        }}>
+                                        Vote
+                                    </button>
+                                </p>
+                            </div>
+                        );
+                    }
+                } else {
+                    for (let k in this.state.voting[i]) {
+                        key[i].push(
+                            <div className="voting" key={k}>
+                                <p>
+                                    {k}: {this.state.voting[i][k]}
+                                    <br />
+                                </p>
+                            </div>
+                        );
+                    }
+                }
+            }
         }
+
+        // for (let k in this.state.voting) {
+        // key.push(
+        // <div className="voting" key={k}>
+        //     <p>
+        //         {k}: {this.state.voting[k]}
+        //         <br />
+        //         <button
+        //             className={`waves-effect waves-light btn ${this.state.voted}`}
+        //             onClick={(e) => {
+        //                 this.handleSubmit(e, k);
+        //             }}>
+        //             Vote
+        //         </button>
+        //     </p>
+        // </div>
+        // );
+        // }
+
+        console.log(this.state.end_display);
+
+        const results = this.state.end_display.map((result, i) => {
+            return (
+                <li key={i}>
+                    {Object.keys(result)[0]} <span>{result[Object.keys(result)[0]]}</span>
+                </li>
+            );
+        });
+
+        const endDisp = (
+            <div className="end-display">
+                <ol>{results}</ol>
+            </div>
+        );
+
         if (this.state.redirect) {
             return <Redirect to="/404" />;
         }
@@ -142,7 +209,7 @@ export default class Bracket extends Component {
         return (
             <div className="Vote">
                 <h2>{this.state.title}?</h2>
-                <div className="container">{key}</div>
+                <div className="container">{this.state.end_display ? endDisp : key}</div>
                 {this.state.voted ? <VoteSubmitted /> : null}
             </div>
         );
